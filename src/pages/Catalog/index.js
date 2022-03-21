8096e9bb86840a3e5713777657ef386e59c90123
caioorleans <caioorleansdm@hotmail.com>
2022-03-21 13:54:15 -0300

    first commit

---
+++ src/pages/Catalog/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Container} from './styles';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+export default function App() {
+  return (
+    <Container/>
+  );
+};
